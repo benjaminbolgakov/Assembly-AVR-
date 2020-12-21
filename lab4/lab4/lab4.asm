cd4745f87db027ace02cef14b9a2bd521a86029e
Benni <benjamin@progrmor.com>
2020-12-21 21:18:27 +0100

    Column-stepping working

--- lab4/lab4/lab4.asm
+++ lab4/lab4/lab4.asm
@@ -6,23 +6,107 @@
 
 	.equ	E			= 1
 
+.dseg
+CUR_POS:	.db $00
+LINE:		.byte 17
+.cseg
+
 	INIT:
 	ldi		r16,HIGH(RAMEND)
 	out		SPH,r16
 	ldi		r16,LOW(RAMEND)
 	out		SPL,r16
+	call	MEM_INIT
 	call	WAIT			;Delay to let LCD start up
 	call	PORT_INIT
 	call	BACKLIGHT_ON	
 	call	FOURBIT_INIT
 	call	DISP_CONFIG		;Blinking cursor at this point
 	call	WAIT
-	call	ADC_READ8
-	call	LCD_PRINT_HEX
 MAIN:
-	;call	KEY_READ
-	;call	LCD_COL
+	;call	LINE_PRINT
+	call	LCD_COL			;Update current column
+	call	SWITCH_BACKLIGHT
+	call	KEY_READ			
+	
+	/*call	LINE_UPDATE
+	ldi		r16,5
+	call	LCD_COL
+	ldi		r16,5
+	call	LCD_COL
+	ldi		r16,3
+	call	LINE_UPDATE
+	ldi		r16,4
+	call	LINE_UPDATE*/
 	jmp		MAIN
+	
+LCD_COL:
+	ldi		XH,HIGH(CUR_POS)
+	ldi		XL,LOW(CUR_POS)
+	cpi		r16,2
+	breq	BACK
+	cpi		r16,5
+	breq	FORWARD
+	ld		r16,X
+	jmp		LCD_COl_DONE
+BACK:
+	ld		r16,X
+	cpi		r16,0b10000000
+	breq	LCD_COL_DONE
+	dec		r16
+	st		X,r16
+	jmp		LCD_COl_DONE
+FORWARD:
+	ld		r16,X
+	cpi		r16,0b10001111
+	breq	LCD_COL_DONE
+	inc		r16
+	st		X,r16
+LCD_COl_DONE:
+	call	LCD_COMMAND
+	ret
+
+LINE_UPDATE:
+	ldi		ZH,HIGH(LINE)
+	ldi		ZL,LOW(LINE)
+	ldi		XH,HIGH(CUR_POS)
+	ldi		XL,LOW(CUR_POS)
+	ld		r20,X			;Loop index to change data on correct position in LINE
+	andi	r20,0b00011111
+	cpi		r16,3
+	breq	BROWSE_DOWN
+	cpi		r16,4
+	breq	BROWSE_UP
+	jmp		UPDATED
+BROWSE_DOWN:
+	ld		r17,Z+			;Get LINE value from current cursor position
+	dec		r20
+	brne	BROWSE_DOWN
+	cpi		r17,' '			;Check for lowest bound
+	breq	UPDATED			
+	cpi		r17,'A'
+	breq	UPDATED
+	dec		r17
+	st		Z,r17
+	jmp		UPDATED
+BROWSE_UP:
+	ld		r17,Z+			;Get LINE value from current cursor position
+	dec		r20
+	brne	BROWSE_UP
+	ld		r17,Z			
+	cpi		r17,'Z'			;Check for highest bound
+	breq	UPDATED	
+	ldi		r20,' '
+	cpse	r17,r20
+	jmp		LETTERS
+	ldi		r17,$41
+	st		Z,r17
+	jmp		UPDATED
+LETTERS:
+	inc		r17
+	st		Z,r17
+UPDATED:
+	ret
 
 KEY_READ:
 	call	KEY
@@ -33,6 +117,7 @@ KEY_WAIT_FOR_PRESS:
 	tst		r16
 	breq	KEY_WAIT_FOR_PRESS
 	ret
+
 KEY:
 	;IDLE =		$FF					=	255
 	;SELECT =	$9F||$AA||$A4||$99	=	159||170
@@ -41,35 +126,26 @@ KEY:
 	;UP =		$18||$11			=	24
 	;RIGHT =	$00					=	0
 	call	ADC_READ8
-	cpi		r16,$FF		;Check for idle state
-	breq	IDLE
 	//Check for RIGHT btn
 	mov		r17,r16
-	ldi		r18,12
-	sub		r17,r18
-	brmi	RIGHT		;0
+	cpi		r17,12
+	brlo	RIGHT		;0
 	//Check for UP btn
 	mov		r17,r16
-	ldi		r18,44
-	sub		r17,r18
-	brmi	UP			;24
+	cpi		r17,44
+	brlo	UP			;24
 	//Check for DOWN btn
 	mov		r17,r16
-	ldi		r18,83
-	sub		r17,r18
-	brmi	DOWN		;64
+	cpi		r17,83
+	brlo	DOWN		;64
 	//Check for LEFT btn
 	mov		r17,r16
-	ldi		r18,130
-	sub		r17,r18
-	brmi	LEFT		;102
-	/*Check for SELECT btn
+	cpi		r17,130
+	brlo	LEFT		;102
+	//Check for SELECT btn
 	mov		r17,r16
-	ldi		r18,207
-	sub		r17,r18
-	brmi	SELECT		;159
-	*/
-	jmp		SELECT
+	cpi		r17,207
+	brlo	SELECT		;159
 IDLE:
 	ldi		r16,0
 	jmp		KEYDONE
@@ -91,12 +167,6 @@ RIGHT:
 KEYDONE:
 	ret
 
-LCD_COL:
-	ldi		r17,0b10000000		;Load prerequisite 
-	or		r16,r17				;Add together with wanted 
-	call	LCD_COMMAND
-	ret
-
 //Analog-Digital convertion
 ADC_READ8:
 	ldi		r16,(1<<REFS0)|(1<<ADLAR)
@@ -114,11 +184,31 @@ ADC_BUSY:
 	lds		r16,ADCH		;Store result to be processed
 	ret
 
+SWITCH_BACKLIGHT:
+	cpi		r16,1
+	brne	SWITCH_DONE
+	sbic	PORTB,2
+	jmp		OFF
+ON:
+	call	BACKLIGHT_ON
+	jmp		SWITCH_DONE
+OFF:
+	call	BACKLIGHT_OFF
+	jmp		SWITCH_DONE
+SWITCH_DONE:
+	ret
+
+LINE_PRINT:
+	call	LCD_HOME
+	ldi		ZH,HIGH(LINE)
+	ldi		ZL,LOW(LINE)
+	call	LCD_PRINT
+	ret
 LCD_PRINT:
 	call	GET_CHAR
 	call	LCD_ASCII
 	cpi		r16,$00			;Determine if the string is empty
-	brne	LCD_PRINT		
+	brne	LCD_PRINT	
 	ret
 GET_CHAR:
 	ld		r16,Z+
@@ -170,6 +260,28 @@ NUMHEX:
 DONE:
 	ret
 
+MEM_INIT:
+	ldi		ZH,HIGH(LINE)
+	ldi		ZL,LOW(LINE)
+	ldi		XH,HIGH(CUR_POS)
+	ldi		XL,LOW(CUR_POS)
+	ldi		r16,0b10000000	;Load prerequisite instruction form
+	st		X,r16			;Store initial cursor position 0
+	ldi		r21,16			;Loop-index LINE
+	ldi		r20,' '
+	call	MEM_WRITE_LINE
+	ret
+//Writes spaces for one full line to SRAM(LINE)
+MEM_WRITE_LINE:
+	st		Z+,r20
+	dec		r21
+	cpi		r21,0
+	brne	MEM_WRITE_LINE
+	ldi		r20,$00
+	st		Z,r20
+	ldi		r21,6			;Set loop-index	TIME
+	ret
+
 //Set write-pos at column 0 (home/start pos)
 LCD_HOME:
 	ldi		r16,0b00000010
@@ -181,6 +293,7 @@ LCD_ERASE:
 	ldi		r16,LCD_CLR
 	call	LCD_COMMAND
 	ret
+
 BACKLIGHT_ON:
 	sbi		PORTB,2
 	ret
